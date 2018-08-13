d2b40aba0ff4d0bb30f9ca7e57212e08c1e91f02
James Kageni <james.kageni@gmail.com>
2018-08-13 14:58:16 +0300

    added the basic service and API call

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms'
+import {FormsModule} from '@angular/forms';
+import {AlertsService} from './alert-service/alerts.service';
+import {HttpClientModule} from '@angular/common/http';
+import { NgProgressModule } from '@ngx-progressbar/core';
+import { NgProgressHttpModule } from '@ngx-progressbar/http';
 
 import { AppComponent } from './app.component';
 import { GoalComponent } from './goal/goal.component';
@@ -21,9 +25,12 @@ import { GoalFormComponent } from './goal-form/goal-form.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    NgProgressModule.forRoot(),
+    NgProgressHttpModule
   ],
-  providers: [],
+  providers: [AlertsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/goal-details/goal-details.component.ts
+++ src/app/goal-details/goal-details.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import {Goal} from '../goal'
+import {Goal} from '../goal';
 
 @Component({
   selector: 'app-goal-details',
@@ -7,10 +7,10 @@ import {Goal} from '../goal'
   styleUrls: ['./goal-details.component.css']
 })
 export class GoalDetailsComponent implements OnInit {
-  @Input() goal:Goal;
-  @Output() isComplete= new EventEmitter<boolean>();
+  @Input() goal: Goal;
+  @Output() isComplete = new EventEmitter<boolean>();
 
-  goalComplete(complete:boolean){
+  goalComplete(complete: boolean) {
     this.isComplete.emit(complete);
   }
   constructor() { }
